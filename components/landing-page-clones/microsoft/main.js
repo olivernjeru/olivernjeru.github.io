4ef8d4629146bab6b6e7b65d7f46ebf51ccf1aac
Oliver Njeru <olivernjiru@gmail.com>
2024-09-12 22:57:04 +0300

    remove landing page clones

--- components/landing-page-clones/microsoft/main.js
+++
@@ -1,2 +0,0 @@
-// get element by class 'menu-btn' and a click event listener to toggle the 'main-menu' class to 'show' 
-document.querySelector('.menu-btn').addEventListener('click', () => document.querySelector('.main-menu').classList.toggle('show'))
