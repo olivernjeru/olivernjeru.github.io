72d640d393eab1d53e641daddfe412babd05c1c5
Oliver Njeru <olivernjiru@gmail.com>
2024-12-18 16:14:04 +0300

    create data stores for projects, awards and certificates
    use data stores for projects, awards and certificates in projects, awards and certificates components

---
+++ src/components/dataStores/Awards.js
@@ -0,0 +1,12 @@
+const awards = [
+    "Dean's List",
+    "JP Morgan Chase Code For Good Africa Virtual Hackathon, September 2023 participant and team lead",
+    "CME Group FX Trading Challenge ranked top 25%, August 2023",
+    "Junior Achievement Africa Scholarship, 2022",
+    "Hult Prize USIU-Africa Innovation Week First Runners Up, 2022",
+    "My Photography works of Art got used 15 times on popular platforms such as Notion, Figma, Padlet, PicsArt, Wordpress & Trello, 2021-2023",
+    "Achieved 1 Million Views for my Photography works of Art on Unsplash, July 20th 2022",
+    "11-time feature on Unsplash, 2021-2022"
+];
+
+export { awards };
---
+++ src/components/dataStores/Projects.js
@@ -0,0 +1,59 @@
+const projects = [
+    {
+      name: 'Ecommerce Website',
+      description: 'A merch store I created for my best friend to increase his sales and customer engagement.',
+      technologies: 'Django, Vuejs, Bulma, HTML',
+      github: 'https://github.com/olivernjeru/ecommercewebsite',
+      live: 'https://github.com/olivernjeru/blindr',
+    },
+    {
+      name: 'Game Collection',
+      description: 'A collection of games including Random Number Guessing Game, TicTacToe, Rock Paper Scissors, and more.',
+      technologies: 'Python CLI',
+      github: 'https://github.com/olivernjeru/explore-python/tree/main/explore-python',
+      live: 'https://olivernjeru.github.io/blindr/blindr.html',
+    },
+    {
+      name: "Multiple Vendor Ecommerce Site",
+      description:
+        "A continuation of the Ecommerce Website project, focusing on Django and enhanced technology stacks.",
+      technologies: "Django, Tailwind, HTML",
+      github:
+        "https://github.com/olivernjeru/explore-python/tree/main/explore-django/multiple-vendors-ecommerce-site",
+      live: "https://olivernjeru.github.io/blindr/blindr.html",
+    },
+    {
+      name: "Explore JavaScript",
+      description:
+        "A collection of JavaScript projects including a TicTacToe Game, foundational vanilla JS concepts, and a CRUD app.",
+      technologies: "Vanilla JavaScript, ReactJS, Tailwind, NodeJS",
+      github: "https://github.com/olivernjeru/explore-javascript",
+      live: "https://olivernjeru.github.io/blindr/blindr.html",
+    },
+    {
+      name: "Landing Page Clones",
+      description:
+        "A collection of landing page clones for popular websites like Microsoft, Hulu, Starbucks, and Netflix, showcasing HTML, CSS, and JavaScript expertise.",
+      technologies: "HTML, CSS, JavaScript",
+      github: "https://github.com/olivernjeru/landing-page-clones",
+      live: "https://olivernjeru.github.io/blindr/blindr.html",
+    },
+    {
+      name: "Blindr",
+      description:
+        "A calculator for the visually impaired that uses event listeners in JavaScript to provide audio feedback for button clicks.",
+      technologies: "JavaScript, HTML, CSS",
+      github: "https://github.com/olivernjeru/blindr",
+      live: "https://olivernjeru.github.io/blindr/blindr.html",
+    },
+    {
+      name: "Parking Lot Management System",
+      description:
+        "An app that checks users into a parking lot, captures their information in a database, and calculates owed amounts upon checkout.",
+      technologies: "Java, MySQL",
+      github: "https://github.com/olivernjeru/parkinglotmanagementsystem",
+      live: "https://olivernjeru.github.io/blindr/blindr.html",
+    },
+  ];
+
+export { projects };
