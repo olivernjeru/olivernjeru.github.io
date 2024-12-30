aeec109ac6b451d0a081039295b0461e8821b178
Oliver Njeru <olivernjiru@gmail.com>
2024-12-30 19:33:08 +0300

    make background color and color dynamic and depended on user's system theme settings

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import { Box, Typography } from '@mui/material';
+import { Box, Typography, useTheme } from '@mui/material';
 
 const Footer = () => {
   const currentYear = new Date().getFullYear(); // Get the current year dynamically
+  const theme = useTheme(); // Get the current theme
 
   return (
-    <Box component="footer" sx={{ backgroundColor: '#333', padding: 2, textAlign: 'center' }}>
-      <Typography variant="h6">Built by Oliver Njeru</Typography>
-      <Typography variant="body2" sx={{ marginTop: 1 }}>
+    <Box component="footer" sx={{ backgroundColor: theme.palette.mode === 'dark' ? '#333' : '#1976D2', padding: 2, textAlign: 'center' }}>
+      <Typography variant="h6" sx={{ color: theme.palette.mode === 'light' ? 'white' : 'inherit' }}>Built by Oliver Njeru</Typography>
+      <Typography variant="body2" sx={{ marginTop: 1, color: theme.palette.mode === 'light' ? 'white' : 'inherit' }}>
         All rights reserved &copy; 2020 - {currentYear}
       </Typography>
     </Box>
