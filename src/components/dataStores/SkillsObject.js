ebabc00cd9c921b5bd733fa2bd7ece636d4da454
Oliver Njeru <olivernjiru@gmail.com>
2024-12-30 14:20:54 +0300

    add Data Analysis, Visulization and Presentation skill

--- src/components/dataStores/SkillsObject.js
+++ src/components/dataStores/SkillsObject.js
@@ -2,5 +2,5 @@ export const categorizedSkills = {
     ProgrammingLanguages: ['Python', 'C++', 'Java', 'JavaScript', 'R', 'Bash', 'C#', 'Assembly Language'],
     SoftwareDevelopmentTools: ['ReactJS', 'Django', 'Flask', 'Vercel', 'Linux', 'MAC OS', 'Git', 'GitHub', 'Windows OS', 'Firebase', 'Pandas', 'Numpy', 'OpenCV', 'Scikit-learn', 'Matplotlib', 'GGPlot', 'MYSQL', 'Unit Testing', 'MongoDB', 'APIs'],
     SoftSkills: ['Research', 'Problem Solving', 'Robust Communication Skills', 'Leadership', 'Resilience', 'Agile Methodology', 'Kanban Methodology', 'Scrum Master', 'Project Management'],
-    OtherSkills: ['User Interface/Experience Design', 'Photography', 'French (Elementary proficiency)', 'Spanish (Limited working proficiency)', 'Swahili (Native or bilingual proficiency)'],
+    OtherSkills: ['User Interface/Experience Design', 'Photography', 'Data Analysis, Visualization and Presentation', 'French (Elementary proficiency)', 'Spanish (Limited working proficiency)', 'Swahili (Native or bilingual proficiency)'],
 };
