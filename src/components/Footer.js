b41740153578bfba976a357eec0d55490470f36c
Oliver Njeru <olivernjiru@gmail.com>
2025-04-23 13:08:35 +0300

    fix: refactor Footer component for improved structure, consistency and readability

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,31 @@
 import React from 'react';
-import { Box, Typography, useTheme } from '@mui/material';
+import {
+  Box,
+  Container,
+  Typography,
+} from '@mui/material';
 
-const Footer = () => {
+export default function Footer() {
   const currentYear = new Date().getFullYear(); // Get the current year dynamically
-  const theme = useTheme(); // Get the current theme
 
   return (
-    <Box component="footer" sx={{ backgroundColor: theme.palette.mode === 'dark' ? '#333' : '#1976D2', padding: 2, textAlign: 'center' }}>
-      <Typography variant="h6" sx={{ color: theme.palette.mode === 'light' ? 'white' : 'inherit' }}>Built by Oliver Njeru</Typography>
-      <Typography variant="body2" sx={{ marginTop: 1, color: theme.palette.mode === 'light' ? 'white' : 'inherit' }}>
-        All rights reserved &copy; 2020 - {currentYear}
-      </Typography>
+    <Box
+      component="footer"
+      sx={{
+        py: 2,
+      }}
+    >
+      {/* Bottom Bar */}
+      <Box>
+        <Container maxWidth="lg" sx={{ textAlign: 'center' }}>
+          <Typography variant='h6'>
+            Built by Oliver Njeru
+          </Typography>
+          <Typography variant="body2">
+            All rights reserved © 2020 – {currentYear}
+          </Typography>
+        </Container>
+      </Box>
     </Box>
   );
-};
-
-export default Footer;
+}
